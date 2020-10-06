ee7ac32ade5029d19a5ee9b09109e1afd401252d
Rodrigo Gross López <rodrigogrosslopez@gmail.com>
2020-10-06 09:30:31 +0200

    Added Redux and version tag component

--- App.js
+++ App.js
@@ -2,12 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Redux imports
+import { Provider } from 'react-redux';
+import configureStore from './src/redux/store';
+
+// Redux declarations
+const store = configureStore();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
