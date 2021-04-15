6d8ce95bb568c6994beca43908de940c5fc5e893
Rodrigo Gross López <rodrigogrosslopez@gmail.com>
2021-04-15 16:11:40 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,75 +1,71 @@
-import React, { useState, useEffect } from 'react'
-import AppLoading from 'expo-app-loading'
-import { StatusBar } from 'expo-status-bar'
-import { enableScreens } from 'react-native-screens'
-import { Provider as PaperProvider } from 'react-native-paper'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
-import AsyncStorage from '@react-native-community/async-storage'
-import theme from './src/theme'
+import React, { useState, useEffect } from "react";
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
+import { enableScreens } from "react-native-screens";
+import { Provider as PaperProvider } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import AsyncStorage from "@react-native-community/async-storage";
+import theme from "./src/theme";
 
 // Redux imports
-import { Provider } from 'react-redux'
-import { setUser, updateDistance } from './src/redux/actions/user.actions'
-import store from './src/redux/store'
-import { initialiseApplication } from './src/redux/actions/application.actions'
+import { Provider } from "react-redux";
+import { setUser, updateDistance } from "./src/redux/actions/user.actions";
+import store from "./src/redux/store";
+import { initialiseApplication } from "./src/redux/actions/application.actions";
 
 // Assets prefetching
-import * as Font from 'expo-font'
-import * as Icon from '@expo/vector-icons'
-import { Asset } from 'expo-asset'
+import * as Font from "expo-font";
+import * as Icon from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 
 // Components imports
-import Navigation from './src/navigation'
+import Navigation from "./src/navigation";
 
-enableScreens()
+enableScreens();
 
 // Redux declarations
-store.dispatch(initialiseApplication())
+store.dispatch(initialiseApplication());
 
 const App = () => {
-  
-  const [isReady, setReady] = useState(false)
-  
-  const loadAssetsAsync = async () =>     
+  const [isReady, setReady] = useState(false);
+
+  const loadAssetsAsync = async () =>
     Promise.all([
-    Asset.loadAsync([
-      require('./assets/icon.png'),
-      require('./src/image/logo.png'),
-    ]),
-    Font.loadAsync({
-      ...Icon.MaterialCommunityIcons.font,
-    }),
-  ])
-  
+      Asset.loadAsync([
+        require("./assets/icon.png"),
+        require("./src/image/logo.png"),
+      ]),
+      Font.loadAsync({
+        ...Icon.MaterialCommunityIcons.font,
+      }),
+    ]);
+
   useEffect(() => {
     async function checkUser() {
-      const response = await AsyncStorage.getItem('user')
-      response && store.dispatch(setUser(JSON.parse(response)))
-      let fecNac = JSON.parse(response).data.fec_nac
-      fecNac && store.dispatch(updateDistance(fecNac))
+      const response = await AsyncStorage.getItem("user");
+      response && store.dispatch(setUser(JSON.parse(response)));
+      let fecNac = JSON.parse(response).data.fec_nac;
+      fecNac && store.dispatch(updateDistance(fecNac));
     }
-    checkUser()
-  }, [])
-  
-  return isReady ? 
-    (
-      <Provider store={store}>
-        <PaperProvider theme={theme}>
-          <SafeAreaProvider>
-            <StatusBar style='auto' />
-            <Navigation />
-          </SafeAreaProvider>
-        </PaperProvider>
-      </Provider>
-    )
-  :
-  (
+    checkUser();
+  }, []);
+
+  return isReady ? (
+    <Provider store={store}>
+      <PaperProvider theme={theme}>
+        <SafeAreaProvider>
+          <StatusBar style="auto" />
+          <Navigation />
+        </SafeAreaProvider>
+      </PaperProvider>
+    </Provider>
+  ) : (
     <AppLoading
       startAsync={loadAssetsAsync}
       onFinish={() => setReady(true)}
       onError={console.warn}
     />
-  )  
-}
+  );
+};
 
-export default App
+export default App;
